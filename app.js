68d1b7c88b5e5aa2e92168bd21d6c988d3f35086
michel <msampil@switch.com.uy>
2023-07-02 21:06:25 -0300

    basic html css

---
+++ app.js
@@ -0,0 +1,41 @@
+let task = [
+  {
+    id: 0,
+    owner: "Pelado Cáceres",
+    name: "Wash the car",
+    description: "Wash the car before crash it 💥🚗",
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSynqaOG2CBeapwLA8A7W3C8kmHhNnNraWl7c2rz1eykm_dY_cjB9erHZawnIFOIo3MbcAts4L7N8W7otPrEPvFmzg9UJo7LONUpVhyPpz1gjDfbMOcetAy52k0YdDDoNaZSQ&usqp=CAc",
+  },
+  {
+    id: 1,
+    owner: "Developer #432",
+    name: "Take humans out of Earth",
+    description: "Looking for a new planet to destroy 🌎",
+    imgUrl:
+      "https://c4.wallpaperflare.com/wallpaper/1020/1/213/world-of-warcraft-battle-for-azeroth-video-games-warcraft-alliance-wallpaper-thumb.jpg",
+  },
+  {
+    id: 2,
+    owner: "Another big fish",
+    name: "Testing in Production",
+    description: `We don't worry about testing, we test in production 🤪`,
+    imgUrl:
+      "https://c4.wallpaperflare.com/wallpaper/246/739/689/digital-digital-art-artwork-illustration-abstract-hd-wallpaper-preview.jpg",
+  },
+  {
+    id: 3,
+    owner: "The return of the Pug",
+    name: "Thinking about all the mankind problems",
+    description: "Eat, Sleep and wear a jedi robe in order to solve it 🐶",
+    imgUrl:
+      "https://w0.peakpx.com/wallpaper/381/236/HD-wallpaper-pug-dog-pet-funny-sad.jpg",
+  },
+  {
+    id: 4,
+    owner: "Mark Zuckerberg",
+    name: "Destroy the entire planet earth",
+    description: "Encourage people to live in a metaverse🌈",
+    imgUrl: "https://pbs.twimg.com/media/Ew2_PGEWgAE3V5-.jpg",
+  },
+];
